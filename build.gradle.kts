f43d99de488618d40628176de1c6daa5712bcbc1
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-04 07:55:22 +0100

    Lagt inn build og workflow

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
+    kotlin("jvm").version(Kotlin.version)
+
+    id(TmsJarBundling.plugin)
+
+    // Apply the application plugin to add support for building a CLI application.
+    application
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+
+
+repositories {
+    mavenCentral()
+    maven {
+        url = uri("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    }
+    mavenLocal()
+}
+
---
+++ buildSrc/src/main/kotlin/groups.kt
@@ -0,0 +1,10 @@
+import default.*
+
+// Managed by tms-dependency-admin. Overrides and additions should be placed in separate file
+
+object Kotlin: KotlinDefaults
+
+object TmsCommonLib: TmsCommonLibDefaults
+object TmsJarBundling {
+    const val plugin = "no.nav.tms.jar-bundling"
+}
