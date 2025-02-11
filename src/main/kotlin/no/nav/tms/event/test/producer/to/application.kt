85c3f711bf0cf68f50ff84b8a14f5de1c3af7c8e
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-11 12:37:16 +0100

    rootPath

--- src/main/kotlin/no/nav/tms/event/test/producer/to/application.kt
+++ src/main/kotlin/no/nav/tms/event/test/producer/to/application.kt
@@ -6,10 +6,12 @@ import no.nav.tms.common.util.config.IntEnvVar.getEnvVarAsInt
 import no.nav.tms.event.test.producer.to.gui.gui
 
 
-fun main() {    embeddedServer(
+fun main() {
+    embeddedServer(
     Netty,
     port = getEnvVarAsInt("PORT", 8081),
     module = {
+        rootPath = "tms-event-test-producer-to"
         gui()
     }
 ).start(wait = true)
