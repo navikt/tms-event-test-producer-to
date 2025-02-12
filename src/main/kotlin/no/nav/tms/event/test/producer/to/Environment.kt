53f3eab57d973a1aba8a11a3be7abddecb6f363a
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-12 18:49:26 +0100

    serialization fix

--- src/main/kotlin/no/nav/tms/event/test/producer/to/Environment.kt
+++ src/main/kotlin/no/nav/tms/event/test/producer/to/Environment.kt
@@ -1,11 +1,13 @@
 package no.nav.tms.event.test.producer.to
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.ktor.client.*
 import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.serialization.jackson.*
 import no.nav.tms.common.util.config.StringEnvVar.getEnvVar
+import java.text.DateFormat
 
 object Environment {
     val navDecoratorenUrl: String = getEnvVar("NAV_DECORATOREN_URL")
@@ -18,6 +20,8 @@ fun HttpClientConfig<*>.configureClient() {
     install(ContentNegotiation) {
         jackson {
             configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            registerModule(JavaTimeModule())
+            dateFormat = DateFormat.getDateTimeInstance()
         }
     }
     install(HttpTimeout) {
