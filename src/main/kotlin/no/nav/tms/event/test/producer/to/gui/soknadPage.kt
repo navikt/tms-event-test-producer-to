6cd141122f455a8c31ea35ed5c09c5d5dede828c
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-12 22:15:49 +0100

    vise mottatt- og manglende-vedlegg på søknad

--- src/main/kotlin/no/nav/tms/event/test/producer/to/gui/soknadPage.kt
+++ src/main/kotlin/no/nav/tms/event/test/producer/to/gui/soknadPage.kt
@@ -1,12 +1,11 @@
 package no.nav.tms.event.test.producer.to.gui
 
-import kotlinx.html.stream.createHTML
-import no.nav.tms.event.test.producer.to.kvittering.fetchEnkelSoknad
-
 import io.ktor.client.*
 import io.ktor.server.routing.*
 import kotlinx.html.*
+import kotlinx.html.stream.createHTML
 import no.nav.tms.event.test.producer.to.Environment
+import no.nav.tms.event.test.producer.to.kvittering.fetchEnkelSoknad
 import no.nav.tms.event.test.producer.to.tokenexchange.TokenFetcher
 import no.nav.tms.token.support.idporten.sidecar.user.IdportenUserFactory
 import java.time.format.DateTimeFormatter
@@ -17,8 +16,8 @@ fun Route.soknadPage(
     soknadskvitteringUrl: String,
     httpClient: HttpClient
 ) {
-    get("soknad/{id}"){
-        val id = call.parameters["id"]?: throw IllegalArgumentException("Parameter 'id' is missing")
+    get("soknad/{id}") {
+        val id = call.parameters["id"] ?: throw IllegalArgumentException("Parameter 'id' is missing")
         val token = IdportenUserFactory.createIdportenUser(call).tokenString
         val soknad =
             fetchEnkelSoknad(tokenFetcher, soknadskvitteringUrl, httpClient, token, id)
@@ -28,13 +27,47 @@ fun Route.soknadPage(
             "Min side søknad test producer", {
                 h1 { +"Detaljer om søknaden" }
                 dl("navds-form-summary__answers") {
-                    unsafe {+ descriptionItems("tittel", soknad.tittel)}
-                    unsafe {+ descriptionItems("temakode", soknad.temakode)}
-                    unsafe {+ descriptionItems("linkSoknad", soknad.linkSoknad)}
-                    unsafe {+ descriptionItems("skjemanummer", soknad.skjemanummer)}
-                    unsafe {+ descriptionItems("journalpostId", soknad.journalpostId)}
-                    unsafe {+ descriptionItems("fristEttersending", soknad.fristEttersending.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")))}
-                    unsafe {+ descriptionItems("dato opprettet", soknad.opprettet.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")))}
+                    unsafe { +descriptionItems("tittel", soknad.tittel) }
+                    unsafe { +descriptionItems("temakode", soknad.temakode) }
+                    unsafe { +descriptionItems("linkSoknad", soknad.linkSoknad) }
+                    unsafe { +descriptionItems("skjemanummer", soknad.skjemanummer) }
+                    unsafe { +descriptionItems("journalpostId", soknad.journalpostId) }
+                    unsafe {
+                        +descriptionItems(
+                            "fristEttersending",
+                            soknad.fristEttersending.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+                        )
+                    }
+                    unsafe {
+                        +descriptionItems(
+                            "dato opprettet",
+                            soknad.opprettet.format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"))
+                        )
+                    }
+                    div("navds-form-summary__answer") {
+                        this@dl.dt("navds-label") { +"Vedlegger motatt" }
+                        this@dl.dd("navds-form-summary__value navds-body-long navds-body-long--medium") {
+                            div {
+                                soknad.mottatteVedlegg.map { mottattVedlegg ->
+                                    ul {
+                                        li{ mottattVedlegg.tittel }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    div("navds-form-summary__answer") {
+                        this@dl.dt("navds-label") { +"Vedlegger mangler" }
+                        this@dl.dd("navds-form-summary__value navds-body-long navds-body-long--medium") {
+                            div {
+                                soknad.manglendeVedlegg.map { mottattVedlegg ->
+                                    ul {
+                                        li{ mottattVedlegg.tittel }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
 
             }, Environment.navDecoratorenUrl
@@ -43,10 +76,9 @@ fun Route.soknadPage(
 }
 
 
-
-fun descriptionItems (label: String, value: String?) = createHTML().run{
-    div("navds-form-summary__answer"){
-        dt("navds-label") { + label}
-        dd("navds-form-summary__value navds-body-long navds-body-long--medium") { + (value ?: "null") }
+fun descriptionItems(label: String, value: String?) = createHTML().run {
+    div("navds-form-summary__answer") {
+        dt("navds-label") { +label }
+        dd("navds-form-summary__value navds-body-long navds-body-long--medium") { +(value ?: "null") }
     }
 }
