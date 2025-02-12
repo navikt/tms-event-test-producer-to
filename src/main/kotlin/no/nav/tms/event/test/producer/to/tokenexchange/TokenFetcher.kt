7345fd23b1b507229bd5b9dbf90530ac80d44e07
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-12 14:26:28 +0100

    Fix 401

--- src/main/kotlin/no/nav/tms/event/test/producer/to/tokenexchange/TokenFetcher.kt
+++ src/main/kotlin/no/nav/tms/event/test/producer/to/tokenexchange/TokenFetcher.kt
@@ -6,9 +6,9 @@ class TokenFetcher(
     private val tokendingsService: TokendingsService,
     private val soknadskvitteringClientId: String
 ) {
-    suspend fun soknadskvitteringToken(token: String) {
+    suspend fun soknadskvitteringToken(token: String) : String{
         try {
-            tokendingsService.exchangeToken(token = token, targetApp = soknadskvitteringClientId)
+            return tokendingsService.exchangeToken(token = token, targetApp = soknadskvitteringClientId)
         } catch (e: Exception) {
             throw Exception("henting av token for soknadskvittering med clientId $soknadskvitteringClientId feiler  ${e.cause}")
         }
