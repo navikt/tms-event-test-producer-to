c9bbb7e406dc0038d3b71c17fe93f8519182dc46
Amirhossien Lotfi <Amirhossien.lotfi@nav.no>
2025-02-13 10:58:15 +0100

    debug

--- src/main/kotlin/no/nav/tms/event/test/producer/to/gui/utils.kt
+++ src/main/kotlin/no/nav/tms/event/test/producer/to/gui/utils.kt
@@ -1,5 +1,7 @@
 package no.nav.tms.event.test.producer.to.gui
 
 fun getStringOrNull(input: String?): String? {
-    return input?.isBlank()?.let { null } ?: input
+    return if (input.isNullOrBlank()) {
+        null
+    } else input
 }
